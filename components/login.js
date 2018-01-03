e24f2553fb6e40ab13d224e52da8d1af90625ec8
Himel Nag Rana <hnrana@gmail.com>
2018-01-03 11:02:21 +0600

    Merged pull request from shiblee

--- components/login.js
+++ components/login.js
@@ -1,65 +1,61 @@
-import React, {Component} from 'react';
-import {View} from 'react-native';
-import Card from './card';
-import CardSection from './card.section';
-import Input from './input';
-import Button from './buttons';
+import React, { Component } from "react";
+import { View } from "react-native";
+import Card from "./card";
+import CardSection from "./card.section";
+import Input from "./input";
+import Button from "./buttons";
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.emailAddress = "";
+    this.password = "";
+  }
 
-    constructor() {
-        super()
-        this.emailAddress = '';
-        this.password = '';
-    }
+  onEmailChange(text) {
+    this.emailAddress = text;
+  }
 
-    onEmailChange(text) {
-        this.emailAddress = text;
-    }
+  onPasswordChange(text) {
+    this.password = text;
+  }
 
-    onPasswordChange(text) {
-        this.password = text;
+  onButtonPress() {
+    if (this.emailAddress.length > 0 && this.password.length > 0) {
+      this.props.goToMemberList();
     }
+  }
 
-    onButtonPress() {
-        if (this.emailAddress.length > 0 && this.password.length > 0) {
-            this.props.goToMemberList();
-        }
-    }
+  componentDidMount() {
+    if (this.props.visible) this.props.titleBarApi("", "Please Login", "");
+  }
 
-    componentDidMount() {
-        if (this.props.visible)
-            this.props.titleBarApi("", "Please Login", "");
+  render() {
+    if (this.props.visible) {
+      return (
+        <Card>
+          <CardSection>
+            <Input
+              label="Email"
+              placeholder="email@gmail.com"
+              onChangeText={this.onEmailChange.bind(this)}
+            />
+          </CardSection>
+          <CardSection>
+            <Input
+              secureTextEntry
+              label="password"
+              placeholder="password"
+              onChangeText={this.onPasswordChange.bind(this)}
+            />
+          </CardSection>
+          <CardSection>
+            <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
+          </CardSection>
+        </Card>
+      );
+    } else {
+      return <View />;
     }
-
-    render() {
-        if (this.props.visible) {
-            return (
-                <Card>
-                    <CardSection>
-                        <Input
-                            label="Email"
-                            placeholder="email@gmail.com"
-                            onChangeText={this.onEmailChange.bind(this)}
-                        />
-                    </CardSection>
-                    <CardSection>
-                        <Input
-                            secureTextEntry
-                            label="password"
-                            placeholder="password"
-                            onChangeText={this.onPasswordChange.bind(this)}
-                        />
-                    </CardSection>
-                    <CardSection>
-                        <Button onPress={this.onButtonPress.bind(this)}>
-                            Login
-                        </Button>
-                    </CardSection>
-                </Card>
-            );
-        } else {
-            return <View/>;
-        }
-    }
+  }
 }
